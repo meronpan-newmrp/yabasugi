08cc17c06478ceb2d420a88ea3795f75b52f563b
めろぱ <chacoco.4989@icloud.com>
2025-11-01 22:15:47 +0900

    Add files via upload

---
+++ send.mjs
@@ -0,0 +1,34 @@
+import fs from "fs";
+import path from "path";
+import express from "express";
+import { Client, Collection, Events, GatewayIntentBits, ActivityType, EmbedBuilder } from "discord.js";
+import CommandsRegister from "./regist-commands.mjs";
+import Notification from "./models/notification.mjs";
+import YoutubeFeeds from "./models/youtubeFeeds.mjs";
+import YoutubeNotifications from "./models/youtubeNotifications.mjs";
+import Sequelize from "sequelize";
+import Parser from 'rss-parser';
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildVoiceStates,
+  ],
+});
+
+client.once('ready', async () => {
+    try {
+        const channel = await client.channels.fetch('1287084831871860880');
+        if (channel && channel.isTextBased()) {
+            await channel.send('シナリオできた？');
+        } else {
+            console.error('指定したチャンネルが見つからないか、テキストチャンネルではありません');
+        }
+    } catch (error) {
+        console.error('エラーが発生しました:', error);
+    }
+});
+
+client.login(process.env.TOKEN);
