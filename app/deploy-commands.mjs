edea7e69a651733983a03a769c781fc54b80b3af
めろぱ <chacoco.4989@icloud.com>
2025-11-02 00:17:46 +0900

    Create deploy-commands.mjs

---
+++ app/deploy-commands.mjs
@@ -0,0 +1,32 @@
+const { REST, Routes } = require('discord.js');
+
+const token = process.env.TOKEN
+const clientId = process.env.APPLICATION_ID
+const guildId = '1298304487605403658'
+
+const fs = require('node:fs');
+
+const commands = [];
+const commandFiles = fs.readdirSync('./gcommands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const command = require(`./gcommands/${file}`);
+  commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '10' }).setToken(token);
+
+(async () => {
+  try {
+    console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+    const data = await rest.put(
+      Routes.applicationGuildCommands(clientId, guildId),
+      { body: commands },
+    );
+
+    console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+  } catch (error) {
+    console.error(error);
+  }
+})();
